56c2f64513591882579c30ecf604bfd91bac3c68
Tarun S <tarun.shiva.sujatha@gmail.com>
2016-08-17 21:43:57 +0530

    Update Pigment_Trans.m

--- Pigment_Trans.m
+++ Pigment_Trans.m
@@ -1,8 +1,4 @@
 function [ l,lg ] = Pigment_Trans( input)
-%pigment transtion Summary of this function goes here
-%Detailed explanation goes here
-%this function used to calculate mean&variance of the gradient of luminace
-%of pigment transtion
 map=imread(input);
 hsv=rgb2hsv(map);
 L=hsv(:,3);
